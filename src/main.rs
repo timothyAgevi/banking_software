28b8985e1d03bc3b16579390205ce46f7d925618
timothyAgevi <timothyagevi@gmail.com>
2022-06-20 18:07:08 +0300

    add withdraw function

--- src/main.rs
+++ src/main.rs
@@ -26,3 +26,20 @@ fn deposit(user: account::Account) -> f32{
     println!("\tSuccesfuly deposited {}$\n\tNew account balance {}$",amount,balance+&amount);
     balance+&amount
 }
+//withdraw function
+fn withdraw(user: account::Account) -> f32{
+    //TODO: Code to identify user and respective balance
+
+    println!("\nEnter amount you wish to withdraw:");
+    let mut amount= String::new();
+    io::stdin().read_line(&mut amount).expect("msg");
+    let amount = amount.trim().parse::<f32>().expect("invalid input");
+    
+    let balance: f32 = user.getBalance(); 
+    if balance-amount>0.0 {
+        println!("\tSuccesfuly withdrew {}$\n\tNew account balance {}$",amount,balance-&amount);
+    }else {
+        println!("\tAvailable amount {}$, insufficient account balance",balance);
+    }
+    balance-&amount
+}
