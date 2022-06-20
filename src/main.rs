f47719c32ad2e91b27ca3cd65e5d45e8f2eeefbc
timothyAgevi <timothyagevi@gmail.com>
2022-06-20 18:05:26 +0300

    add register function

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,12 @@ fn delete(){}
 
 fn transfer(){}
 fn quit(){}
+//register function
+fn register() -> account::Account{
+    println!("\nWelcome to the registration page.\nPlease enter your name to create an account:");
+    let mut name= String::new();
+    io::stdin().read_line(&mut name).expect("msg");
+    let user: account::Account = account::Account::new(&name);
+    println!("\tCreated user {}\tAccount balance: 0",name);
+    user
+}
